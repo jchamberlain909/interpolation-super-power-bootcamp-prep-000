4be0dbf5185bd3a8144001c0cb5cce13ecbdf3d1
Jared Chamberlain <jchamberlain909@gmail.com>
2018-05-01 17:58:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,5 @@ def display_rainbow (colorsArray)
   print "R: #{colorsArray[0]}, O: #{colorsArray[1]}, Y: #{colorsArray[2]}, G: #{colorsArray[3]}, B: #{colorsArray[4]}, I: #{colorsArray[5]}, V: #{colorsArray[6]}"
 
 end
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
